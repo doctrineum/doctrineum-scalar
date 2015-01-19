74c7eb7761dceec12c5212f5a72710b11d4c2e00
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-01-19 14:46:54 +0100

    EnumType conversions covered by tests

--- tests/Doctrineum/EnumTypeTest.php
+++ tests/Doctrineum/EnumTypeTest.php
@@ -71,4 +71,131 @@ class EnumTypeTest extends \PHPUnit_Framework_TestCase
         $enumType = EnumType::getType(EnumType::TYPE);
         $enumType->convertToDatabaseValue('foo', \Mockery::mock(AbstractPlatform::class));
     }
+
+    /**
+     * @test
+     */
+    public function null_to_php_value_is_null()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $this->assertNull($enumType->convertToPHPValue(null, \Mockery::mock(AbstractPlatform::class)));
+    }
+
+    /**
+     * @test
+     */
+    public function string_to_php_value_is_enum_with_that_string()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $enum = $enumType->convertToPHPValue($string = 'foo', \Mockery::mock(AbstractPlatform::class));
+        $this->assertInstanceOf(Enum::class, $enum);
+        $this->assertSame($string, $enum->getValue());
+    }
+
+    /**
+     * @test
+     */
+    public function empty_string_to_php_value_is_enum_with_that_empty_string()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $enum = $enumType->convertToPHPValue($string = '', \Mockery::mock(AbstractPlatform::class));
+        $this->assertInstanceOf(Enum::class, $enum);
+        $this->assertSame($string, $enum->getValue());
+    }
+
+    /**
+     * The Enum class does NOT cast non-string scalars into string (integers, floats etc).
+     * (But saving the value into database and pulling it back probably will.)
+     *
+     * @test
+     */
+    public function integer_to_php_value_is_enum_with_that_integer()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $enum = $enumType->convertToPHPValue($integer = 12345, \Mockery::mock(AbstractPlatform::class));
+        $this->assertInstanceOf(Enum::class, $enum);
+        $this->assertSame($integer, $enum->getValue());
+    }
+
+    /**
+     * The Enum class does NOT cast non-string scalars into string (integers, floats etc).
+     * (But saving the value into database and pulling it back probably will.)
+     *
+     * @test
+     */
+    public function zero_integer_to_php_value_is_enum_with_that_zero_integer()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $enum = $enumType->convertToPHPValue($integer = 0, \Mockery::mock(AbstractPlatform::class));
+        $this->assertInstanceOf(Enum::class, $enum);
+        $this->assertSame($integer, $enum->getValue());
+    }
+
+    /**
+     * The Enum class does NOT cast non-string scalars into string (integers, floats etc).
+     * (But saving the value into database and pulling it back probably will.)
+     *
+     * @test
+     */
+    public function float_to_php_value_is_enum_with_that_float()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $enum = $enumType->convertToPHPValue($float = 12345.6789, \Mockery::mock(AbstractPlatform::class));
+        $this->assertInstanceOf(Enum::class, $enum);
+        $this->assertSame($float, $enum->getValue());
+    }
+
+    /**
+     * The Enum class does NOT cast non-string scalars into string (integers, floats etc).
+     * (But saving the value into database and pulling it back probably will.)
+     *
+     * @test
+     */
+    public function zero_float_to_php_value_is_enum_with_that_zero_float()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $enum = $enumType->convertToPHPValue($float = 0, \Mockery::mock(AbstractPlatform::class));
+        $this->assertInstanceOf(Enum::class, $enum);
+        $this->assertSame($float, $enum->getValue());
+    }
+
+    /**
+     * The Enum class does NOT cast non-string scalars into string (integers, floats etc).
+     * (But saving the value into database and pulling it back probably will.)
+     *
+     * @test
+     */
+    public function false_to_php_value_is_enum_with_that_false()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $enum = $enumType->convertToPHPValue($false = false, \Mockery::mock(AbstractPlatform::class));
+        $this->assertInstanceOf(Enum::class, $enum);
+        $this->assertSame($false, $enum->getValue());
+    }
+
+    /**
+     * The Enum class does NOT cast non-string scalars into string (integers, floats etc).
+     * (But saving the value into database and pulling it back probably will.)
+     *
+     * @test
+     */
+    public function true_to_php_value_is_enum_with_that_true()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $enum = $enumType->convertToPHPValue($true = true, \Mockery::mock(AbstractPlatform::class));
+        $this->assertInstanceOf(Enum::class, $enum);
+        $this->assertSame($true, $enum->getValue());
+    }
+
+    /**
+     * @test
+     * @expectedException
+     */
+    public function non_scalar_to_php_value_cause_exception()
+    {
+        $enumType = EnumType::getType(EnumType::TYPE);
+        $enum = $enumType->convertToPHPValue($true = true, \Mockery::mock(AbstractPlatform::class));
+        $this->assertInstanceOf(Enum::class, $enum);
+        $this->assertSame($true, $enum->getValue());
+    }
 }
