56d75811653797ae660a4e0b372c8ed18ef9cfd2
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-01-19 12:57:55 +0100

    Method unused default value removed.
    Because of already removed typehinting the null as a default value does not have sense anymore.

--- Doctrineum/EnumType.php
+++ Doctrineum/EnumType.php
@@ -47,19 +47,19 @@ class EnumType extends Type
     /**
      * Convert enum instance to database string value
      *
-     * @param Enum $value
+     * @param Enum|null $value
      * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
      * @return string
      * @throws Exceptions\Logic
      */
-    public function convertToDatabaseValue($value = null, AbstractPlatform $platform)
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
     {
         if (is_null($value)) {
             return null;
         }
 
         if (!is_a($value, Enum::class)) {
-            throw new Exceptions\Logic('Unexpected value. Expected ' . Enum::class. ', got ' . gettype($value));
+            throw new Exceptions\Logic('Unexpected value. Expected ' . Enum::class . ', got ' . gettype($value));
         }
 
         return $value->getValue();
