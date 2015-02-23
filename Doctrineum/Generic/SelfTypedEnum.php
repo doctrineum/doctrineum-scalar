305d78e33533399e438b0447a7ff9c00efffa9a6
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-02-23 14:16:59 +0100

    Self typed enum added

---
+++ Doctrineum/Generic/SelfTypedEnum.php
@@ -0,0 +1,18 @@
+<?php
+namespace Doctrineum\Generic;
+
+use Granam\Strict\Object\StrictObjectTrait;
+
+class SelfTypedEnum extends EnumType implements EnumInterface
+{
+    use StrictObjectTrait;
+    use EnumTrait;
+
+    /**
+     * @param int|float|string|bool|null $enumValue
+     */
+    public function __construct($enumValue)
+    {
+        $this->enumValue = $enumValue;
+    }
+}
