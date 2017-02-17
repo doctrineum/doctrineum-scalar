62b50d49d119e776165a6cb40a69a0da27510538
Jaroslav Tyc <mail@jaroslavtyc.com>
2017-02-17 08:40:15 +0100

    Fixed exceptions hierarchy tests

--- Doctrineum/Tests/Scalar/ExceptionsHierarchyTest.php
+++ Doctrineum/Tests/Scalar/ExceptionsHierarchyTest.php
@@ -1,20 +1,29 @@
 <?php
 namespace Doctrineum\Tests\Scalar;
 
-use Granam\Tests\Exceptions\Tools\AbstractExceptionsHierarchyTest;
+use Granam\Tests\ExceptionsHierarchy\Exceptions\AbstractExceptionsHierarchyTest;
 
 class ExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
 {
+    /**
+     * @return string
+     */
     protected function getTestedNamespace()
     {
         return $this->getRootNamespace();
     }
 
+    /**
+     * @return string
+     */
     protected function getRootNamespace()
     {
         return str_replace('\Tests', '', __NAMESPACE__);
     }
 
+    /**
+     * @return string
+     */
     protected function getExternalRootNamespaces()
     {
         return 'Granam\Scalar';
